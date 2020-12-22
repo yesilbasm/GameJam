050f51195d2d5165f134024d8074962e480be418
Mustafa Zübeyr Yeşilbaş <mustafazubeyryesilbas@gmail.com>
2020-12-23 02:46:06 +0300

    SoundSystem Implemented

---
+++ Kodluyoruz-GameJam-main/Assets/[Game]/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿  
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    private AudioSource playerAudioSource;
+    public AudioSource PlayerAudioSource
+    {
+        get
+        {
+            if(playerAudioSource==null)
+            {
+                playerAudioSource=GetComponent<AudioSource>();
+            }
+            return playerAudioSource;
+        }
+    }
+    
+    
+    public AudioClip ScoreUpSound;
+    public AudioClip LooseHealthSound;
+    public AudioClip ThemeSound;
+    public AudioClip HealUpSound;
+    public AudioClip GameOverSound;
+    
+    
+
+
+
+}
+  
---
+++ Kodluyoruz-GameJam-main/Assets/[Game]/Scripts/Player/SoundConroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundConroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnEnable() 
+    {
+        EventManager.OnScore.AddListener(ScoreUpSound);
+        EventManager.OnMiss.AddListener(HealUpSound);
+        EventManager.OnGameOver.AddListener(GameOverSound);
+        EventManager.OnGameStart.RemoveListener(GameStartSound);
+        EventManager.OnHealUp.AddListener(HealUpSound);
+    }
+
+    private void OnDisable() 
+    {
+        EventManager.OnScore.RemoveListener(ScoreUpSound);
+        EventManager.OnMiss.RemoveListener(HealUpSound);
+        EventManager.OnGameOver.RemoveListener(GameOverSound);
+        EventManager.OnGameStart.RemoveListener(GameStartSound);
+        EventManager.OnHealUp.RemoveListener(HealUpSound);
+    }
+
+
+
+    private void ScoreUpSound ()
+    {
+        SoundManager.Instance.PlayerAudioSource.PlayOneShot(SoundManager.Instance.ScoreUpSound,0.5f);
+        Debug.Log("ScoreUp");
+
+    }
+
+    private void HealUpSound()
+    {
+        SoundManager.Instance.PlayerAudioSource.PlayOneShot(SoundManager.Instance.HealUpSound,0.5f);
+        Debug.Log("Hea");
+    } 
+
+    private void LooseHealthSound()
+    {
+        SoundManager.Instance.PlayerAudioSource.PlayOneShot(SoundManager.Instance.LooseHealthSound,0.5f);
+        Debug.Log("LooseHealth");
+    }
+
+    private void GameOverSound()
+    {
+        SoundManager.Instance.PlayerAudioSource.PlayOneShot(SoundManager.Instance.LooseHealthSound,0.5f);
+         SoundManager.Instance.PlayerAudioSource.Stop();
+        Debug.Log("GameOver");
+    }
+
+    private void GameStartSound()
+    {
+        SoundManager.Instance.PlayerAudioSource.Play();
+        Debug.Log("GameStart");
+    }
+
+    
+}
